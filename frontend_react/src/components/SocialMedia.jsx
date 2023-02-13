b1269c9ac9a91e93b5927e0e09828dd0359fe2f3
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2023-02-12 22:56:24 -0700

    added resume...which needs updated

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaPaperclip } from 'react-icons/fa';
+import resume from '../assets/resume.pdf'
 
 const SocialMedia = () => (
   <div className="app__social">
@@ -15,11 +16,17 @@ const SocialMedia = () => (
       </a>
     </div>
     <div>
-      <a href="mailto:btschu@icloud.com">
+      <a href="mailto:btschu@icloud.com" title="Email Me">
         <AiOutlineMail />
       </a>
     </div>
+    <div>
+      <a href={resume} target="_blank" rel="noreferrer" title="Download My Resume" download>
+        <FaPaperclip />
+      </a>
+    </div>
   </div>
+  
 );
 
 export default SocialMedia;
