75d52491665377702d4b060bba9cb0ebe19a62e5
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2023-02-15 20:08:56 -0700

    new react icon for resume

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
-import { FaGithub, FaLinkedin, FaPaperclip } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { GrDocumentPdf } from 'react-icons/gr';
 import resume from '../assets/resume.pdf'
 
 const SocialMedia = () => (
@@ -8,7 +9,7 @@ const SocialMedia = () => (
       <button><a href="https://github.com/btschu" target="_blank" rel="noreferrer" title="View my GitHub" aria-label="View my GitHub"><FaGithub /><span>GitHub</span></a></button>
       <button><a href="https://www.linkedin.com/in/brandon-schumacher/" target="_blank" rel="noreferrer" title="View My LinkedIn" aria-label="View My LinkedIn"><FaLinkedin /><span>LinkedIn</span></a></button>
       <button><a href="mailto:btschu@icloud.com" title="Email Me" aria-label="Email Me"><AiOutlineMail /><span>Email</span></a></button>
-      <button><a href={resume} target="_blank" rel="noreferrer" title="Download My Resume" aria-label="Download My Resume"><FaPaperclip /><span>Resume</span></a></button>
+      <button><a href={resume} target="_blank" rel="noreferrer" title="Download My Resume" aria-label="Download My Resume"><GrDocumentPdf /><span>Resume</span></a></button>
   </div>
   
 );
