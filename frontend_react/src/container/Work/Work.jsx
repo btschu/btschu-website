157e87a58156c54869b25f43d250b95f0faa4fae
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2023-02-15 21:05:36 -0700

    fixed cards

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -96,7 +96,7 @@ const Work = () => {
             <div className="app__work-content app__flex">
               <h4 className="bold-text" style={{ marginTop: 5, fontSize: 20, color: '#313bac'}}>{work.title}</h4>
               <p className="p-text" style={{ fontSize: 15, marginTop: 10 }}>{work.description}</p>
-              <p className="p-text" style={{ fontSize: 13, marginTop: 10, color: '#313bac' }}><em>{work.technologies}</em></p>
+              <p className="p-text" style={{ fontSize: 13, marginTop: 10, marginBottom: 10, color: '#313bac' }}><em>{work.technologies}</em></p>
 
               <div className="app__work-tag app__flex">
                 <p style={{ fontSize: 12 }}>{work.tags[0]}</p>
