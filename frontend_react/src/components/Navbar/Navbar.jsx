9966eaec48c1466d62d80c1fbcdf66734b6747ed
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2023-02-23 13:16:44 -0700

    click out of menu closes the menu

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,58 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { HiMenuAlt4, HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion';
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+import ClickAwayListener from "@mui/base/ClickAwayListener";
 
-import logo from '../../assets/logo.png';
-import logoDark from '../../assets/logo-dark.png';
+import logo from "../../assets/logo.png";
+import logoDark from "../../assets/logo-dark.png";
 
-import './Navbar.scss';
+import "./Navbar.scss";
 
-const Navbar = ({theme}) => {
+const Navbar = ({ theme }) => {
   const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="app__navbar">
-
       <div className="app__navbar-logo">
         <a href="#home">
-          {theme === "dark" ? <img src={logoDark} alt="logo" /> : <img src={logo} alt="logo"/>}
+          {theme === "dark" ? (
+            <img src={logoDark} alt="logo" />
+          ) : (
+            <img src={logo} alt="logo" />
+          )}
         </a>
       </div>
       <ul className="app__navbar-links">
-        {['home', 'about', 'portfolio', 'skills', 'testimonials', 'contact'].map((item, index) => (
-          <li className="app__flex p-text" key={`link-${item}`} tabIndex={index + 1}>
+        {[
+          "home",
+          "about",
+          "portfolio",
+          "skills",
+          "testimonials",
+          "contact",
+        ].map((item, index) => (
+          <li
+            className="app__flex p-text"
+            key={`link-${item}`}
+            tabIndex={index + 1}
+          >
             <div />
-            <a href={`#${item}`} aria-label={item}>{item}</a>
+            <a href={`#${item}`} aria-label={item}>
+              {item}
+            </a>
           </li>
         ))}
       </ul>
+      <ClickAwayListener onClickAway={() => setToggle(false)}>
+        <div className="app__navbar-menu">
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
 
-      <div className="app__navbar-menu">
-          
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
-          
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.5, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            
-            <ul>
-              {['home', 'about', 'portfolio', 'skills', 'testimonials', 'contact'].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false) }>
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
+          {toggle && (
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.5, ease: "easeOut" }}
+            >
+              <HiX onClick={() => setToggle(false)} />
 
-          </motion.div>
-        )}
-      </div>
-        
+              <ul>
+                {[
+                  "home",
+                  "about",
+                  "portfolio",
+                  "skills",
+                  "testimonials",
+                  "contact",
+                ].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          )}
+        </div>
+      </ClickAwayListener>
     </nav>
   );
 };
