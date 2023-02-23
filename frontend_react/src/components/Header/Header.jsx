c276dcdf8082256ecbb8ee62619a1ccd0db9a7f2
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2023-02-23 10:41:56 -0700

    Changed folders and fixed props to header section

---
+++ frontend_react/src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+// import { AppWrap } from "../../wrapper";
+import "./Header.scss";
+import profileImg from '../../assets/profile.png'
+import profileImgDark from '../../assets/profile-dark.png'
+
+const Header = ({theme}) => {
+
+  return (
+    <div className="app__header">
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+        className="app__header-info"
+      >
+        <div className="app__header-container">
+          <div className="app__header-info">
+            <h3 className="bold-text">Welcome!</h3>
+            <h1 className="head-text">
+            As a <span>full-stack developer</span>, I have the ability to create solutions that seamlessly integrate various layers of technology and functionality          </h1>
+          </div>
+          <div className="app__header-img">
+            {theme === "dark" ? <img src={profileImgDark} alt="profile_picture" /> : <img src={profileImg} alt="profile_picture"/>}
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  )
+};
+
+export default Header;
